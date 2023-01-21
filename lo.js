1ca4f6e03f92e2158e7a19874a5384b8e48223e3
伊吹 <reirai2387rinurinu@gmail.com>
2023-01-21 22:48:47 +0900

    Update lo.js

--- lo.js
+++ lo.js
@@ -1,5 +1,5 @@
 window.onload = function(){
     sleep(2000);
     1
-location.href = 'https://muranakahp.netlify.app/ito.html';
+window.location.href = 'https://muranakahp.netlify.app/ito.html';
     }
